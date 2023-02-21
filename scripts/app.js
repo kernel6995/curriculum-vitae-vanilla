c0786575edccdb4074f42a9b139e612ac8dee8df
kernel6995 <pfd76995@gmail.com>
2023-02-21 02:43:50 -0300

    nuevos cambios

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,4 @@
-/* ============================== FUNCIONES PARA EL MENU DE NAVEGACION ============================== */
+/* ===== FUNCIONES PARA EL MENU DE NAVEGACION ===== */
 window.addEventListener('scroll', function () {
     const header = MenuNavegacion;
     // const header = document.querySelector('header');
@@ -11,4 +11,47 @@ function menuToggle() {
     const navigation = document.querySelector('.navigation');
     menuToggle.classList.toggle('active');
     navigation.classList.toggle('active');
+}
+
+/* ===== FUNCIONES LA VENTANA MODAL DE TECNOLOGIAS ===== */
+
+const storeStudies = [
+    {
+        _id: 1,
+        name: 'titulo',
+        src: '',
+    },
+    {
+        _id: 2,
+        name: 'titulo 2',
+        src: '',
+    },
+    {
+        _id: 3,
+        name: 'titulo 3',
+        src: '',
+    },
+    {
+        _id: 4,
+        name: 'titulo 4',
+        src: '',
+    },
+    {
+        _id: 5,
+        name: 'titulo 5',
+        src: './images/R.jpg',
+    },
+];
+
+function abrirModal(id) {
+    MiModal.classList.add('active');
+
+    const estudio = storeStudies.find(e => e._id === id);
+    document.querySelector('#MiModal h3').innerHTML = estudio.name;
+
+    document.querySelector('#MiModal img').src ='./images/background_baner.jpg';
+}
+
+function cerrarModal() {
+    MiModal.classList.remove('active');
 }
